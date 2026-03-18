c13388f3d787eff41339e13581f2607534b8d681
unknown <madudsdtan@gmail.com>
2026-03-18 20:11:38 -0300

    primeiro commit

---
+++ src/main/java/com/lachimia/saito/SaitoApplication.java
@@ -0,0 +1,13 @@
+package com.lachimia.saito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lachimia/saito/SaitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lachimia.saito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
